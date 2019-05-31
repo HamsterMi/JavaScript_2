37f4f733c14a4e666e51c3573bc9a87eaba97e78
liza <lisaminakova@gmail.com>
2019-05-31 15:44:11 +0300

    выполнено домашнее задание №6

--- main.js
+++ main.js
@@ -3,14 +3,58 @@
 const API_URL =
   "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 
+Vue.component("goods-list", {
+  props: ["goods"],
+  template: `
+<div class="goods-list">
+<goods-item v-for="good in goods" :good="good" :key=good.id_product></goods-item>
+</div>`
+});
+
+Vue.component("goods-item", {
+  props: ["good"],
+  template: `
+<div class="goods-item">
+<div class="goods-item__wrap">
+<h3 class="goods-item__title">{{good.product_name}}</h3>
+<p class="goods-item__text">{{good.price}}</p>
+</div>
+</div>
+`
+});
+
+Vue.component("cart", {
+  props: ["visible"],
+  template: `
+  <div class="cart" v-if="visible">
+              Ваша корзина еще пуста
+            </div>`
+});
+
+Vue.component("search-form", {
+  data: () => ({
+    searchLine: ""
+  }),
+  methods: {
+    onSubmit() {
+      this.$emit("submit", this.searchLine);
+    }
+  },
+  template: `
+  <form class="search-form" @submit.prevent="onSubmit">
+  <input class="goods-search" type="text" v-model.trim="searchLine"/>
+  <button class="search-button button" type="submit">
+    Искать
+  </button>
+</form>`
+});
+
 const app = new Vue({
   el: "#app",
   data: {
     goods: [],
     filteredGoods: [],
-    searchLine: "",
-    isVisibleCart: false,
-    
+    isVisibleCart: false
   },
   methods: {
     makeGETRequest(url) {
@@ -37,8 +81,8 @@ const app = new Vue({
         xhr.send();
       });
     },
-    filterGoods() {
-      const REGEXP = new RegExp(this.searchLine, "i");
+    filterGoods(searchLine) {
+      const REGEXP = new RegExp(searchLine, "i");
       this.filteredGoods = this.goods.filter(good =>
         REGEXP.test(good.product_name)
       );
@@ -51,9 +95,7 @@ const app = new Vue({
   mounted() {
     this.makeGETRequest(` ${API_URL}/catalogData.json`).then(goods => {
       this.goods = goods;
-      this.filteredGoods =goods;
-    /*   this.filteredGoods =
-        this.filteredGoods.lenght > 0 ? this.filteredGoods : goods; */
+      this.filteredGoods = goods;
     });
   }
 });
